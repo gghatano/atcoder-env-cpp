95ee28fd986d63bf9638d9321f7f5c81df5fea27
gghatano <adad.0405@gmail.com>
2022-01-30 17:19:21 +0900

    Update algorithm.cpp

--- algorithm.cpp
+++ algorithm.cpp
@@ -154,53 +154,55 @@ char toupper(char c) {
 }
 
 // ダイクストラ
+// ダイクストラ
 # define REP(i,n) for (int i=0;i<(n);++i)
 struct edge{ll to, cost;};
 typedef pair<ll,ll> P;
+
 struct graph{
-  ll V;
-  vector<vector<edge> > G;
-  vector<ll> d;
- 
-  graph(ll n){
-    init(n);
-  }
- 
-  void init(ll n){
-    V = n;
-    G.resize(V);
-    d.resize(V);
-    REP(i,V){
-      d[i] = INF;
+    ll V;
+    vector<vector<edge> > G;
+    vector<ll> d;
+    graph(ll n){
+        init(n);
     }
-  }
  
-  void add_edge(ll s, ll t, ll cost){
-    edge e;
-    e.to = t, e.cost = cost;
-    G[s].push_back(e);
-  }
- 
-  void dijkstra(ll s){
-    REP(i,V){
-      d[i] = INF;
-    }
-    d[s] = 0;
-    priority_queue<P,vector<P>, greater<P> > que;
-    que.push(P(0,s));
-    while(!que.empty()){
-      P p = que.top(); que.pop();
-      ll v = p.second;
-      if(d[v]<p.first) continue;
-      for(auto e : G[v]){
-        if(d[e.to]>d[v]+e.cost){
-          d[e.to] = d[v]+e.cost;
-          que.push(P(d[e.to],e.to));
+    void init(ll n){
+        V = n;
+        G.resize(V);
+        d.resize(V);
+        REP(i,V){
+            d[i] = INF;
         }
-      }
     }
-  }
+    
+    void add_edge(ll s, ll t, ll cost){
+        edge e;
+        e.to = t, e.cost = cost;
+        G[s].push_back(e);
+    }
+
+    void dijkstra(ll s){
+        REP(i,V){
+            d[i] = INF;
+        }
+        d[s] = 0;
+        priority_queue<P,vector<P>, greater<P> > que;
+        que.push(P(0,s));
+        while(!que.empty()){
+            P p = que.top(); que.pop();
+            ll v = p.second;
+            if(d[v]<p.first) continue;
+            for(auto e : G[v]){
+                if(d[e.to]>d[v]+e.cost){
+                    d[e.to] = d[v]+e.cost;
+                    que.push(P(d[e.to],e.to));
+                }
+            }
+        }
+    }
 };
+
 
 // LCS:最長共通部分文字列
 string LCS(string s, string t){
